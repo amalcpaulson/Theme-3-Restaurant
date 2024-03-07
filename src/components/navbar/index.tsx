2d3762cbc6a4daf93f9026d547b6ef9460182328
Amal C P <acp070146@gmail.com>
2024-03-07 11:04:01 +0530

    navbar section completed

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,8 +1,20 @@
-
-type Props = {}
+import styles from "./index.module.css";
+import Logo from "../../assets/Logo.png";
+import { Cart } from "../../assets/svg";
+type Props = {};
 
 export const Navbar = (_props: Props) => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className={styles.Wrapper}>
+      <button className={styles.Logo}>
+        <img src={Logo} alt="" />
+      </button>
+      <div className={styles.ButtonWrapper}>
+        <button className={styles.CartBtn}>
+          <Cart /> <p>4 items</p>
+        </button>
+        <button className={styles.LoginBtn}>Login</button>
+      </div>
+    </div>
+  );
+};
