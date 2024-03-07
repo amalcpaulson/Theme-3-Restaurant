7a03681caf46cfb679868dd51168f0f8ac7b7105
Amal C P <acp070146@gmail.com>
2024-03-07 10:29:32 +0530

    footer section completed

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,8 +1,54 @@
-
-type Props = {}
+import styles from "./index.module.css";
+import { FaInstagram, FaFacebook, FaTwitter } from "react-icons/fa";
+import iphone from "./iPhone.png";
+type Props = {};
 
 export const Footer = (_props: Props) => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className={styles.Wrapper}>
+      <div className={styles.Contact}>
+        <p>Contact</p>
+        <div>
+          <a href="">Help & Support</a>
+          <a href="">Partner with us</a>
+          <a href="">Ride with us</a>
+        </div>
+      </div>
+      <div className={styles.working}>
+        <h3>WORKING HOURS</h3>
+        <div>
+          <p>
+            <span className="colorText">Mon - Fri</span>:7.00am - 6.00pm
+          </p>
+          <p>
+            <span className="colorText">Sat</span>:7.00am - 6.00pm
+          </p>{" "}
+          <p>
+            <span className="colorText">Sun</span>:8.00am - 6.00pm
+          </p>
+        </div>
+      </div>
+      <div className={styles.Online}>
+        <div>
+          <h2>Online ordering is supported on the mobile app</h2>
+          <button>download the app</button>
+        </div>
+        <img src={iphone} alt="" />
+      </div>
+      <div className={styles.Social}>
+        <p>Follow Us</p>
+        <div>
+          <a href="">
+            <FaInstagram />
+          </a>
+          <a href="">
+            <FaFacebook />
+          </a>
+          <a href="">
+            <FaTwitter />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/footer/svg.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-type Props = {}
-
-const svg = (props: Props) => {
-  return (
-    <div>svg</div>
-  )
-}
