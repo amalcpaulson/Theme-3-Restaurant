1a689024f40b79b83b8abb01411a71722ae8b028
Amal C P <acp070146@gmail.com>
2024-03-09 01:29:46 +0530

    delivery page done

--- src/pages/deliveryAddress/components/payments/index.tsx
+++ src/pages/deliveryAddress/components/payments/index.tsx
@@ -1,9 +1,51 @@
-import styles from './index.module.css'
+import styles from "./index.module.css";
+import { COD, CardPayment, Gpay, Netbanking } from "./svg";
 
-type Props = {}
+type Props = {};
 
 export const Payments = (_props: Props) => {
   return (
-    <div className={styles.Wrapper}>Payments</div>
-  )
-}
+    <div className={styles.Wrapper}>
+      <h1>Payment</h1>
+      <div>
+        <div>
+          <div>
+            {" "}
+            <input id="cardPayment" type="checkbox" />
+            <CardPayment />
+          </div>
+          <label htmlFor="cardPayment">Card Payment</label>
+        </div>
+        <div>
+          <div>
+            {" "}
+            <input id="gpay" type="checkbox" />
+            <Gpay />
+          </div>
+          <label htmlFor="gpay">Google Pay</label>
+        </div>{" "}
+        <div>
+          <div>
+            {" "}
+            <input id="netbanking" type="checkbox" />
+            <Netbanking />
+          </div>
+          <label htmlFor="netbanking">Netbanking</label>
+        </div>{" "}
+        <div>
+          <div>
+            {" "}
+            <input id="cod" type="checkbox" />
+            <COD />
+          </div>
+          <label htmlFor="cod">Cash on Delivery</label>
+        </div>
+      </div>
+      <p>
+        By making this purchase you agree to our terms and conditions . I agree
+        that placing the order places me under an obligation to make a payment
+        in accordance with the General Terms and Conditions
+      </p>
+    </div>
+  );
+};
