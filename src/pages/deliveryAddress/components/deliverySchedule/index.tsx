b4b77ebc7ae4f4f66963eb8e5601161bba95bf08
Amal C P <acp070146@gmail.com>
2024-03-09 00:37:25 +0530

    delivery page updated

--- src/pages/deliveryAddress/components/deliverySchedule/index.tsx
+++ src/pages/deliveryAddress/components/deliverySchedule/index.tsx
@@ -3,7 +3,56 @@ import styles from './index.module.css'
 type Props = {}
 
 export const DeliverySchedule = (_props: Props) => {
+  const data = [
+    {
+      day: "Today",
+      time: "9:00PM to 10:00PM",
+    },
+    {
+      day: "Today",
+      time: "10:00PM to 11:00PM",
+    },
+    {
+      day: "Today",
+      time: "11:00PM to 12:00PM",
+    },
+    {
+      day: "Today",
+      time: "12:00PM to 01:00PM",
+    },
+    {
+      day: "Today",
+      time: "01:00PM to 02:00PM",
+    },
+    {
+      day: "Today",
+      time: "03:00PM to 04:00PM",
+    },
+    {
+      day: "Today",
+      time: "05:00PM to 06:00PM",
+    },
+    {
+      day: "Today",
+      time: "06:00PM to 07:00PM",
+    },
+    {
+      day: "Today",
+      time: "07:00PM to 08:00PM",
+    },
+  ];
   return (
-    <div className={styles.Wrapper}>deliverySchedule</div>
-  )
+    <div className={styles.Wrapper}>
+      <h1>Delivery Schedule</h1>
+      <p>Please select a delivery time</p>
+      <div>
+        {data.map(({ day, time }) => (
+          <div key={time}>
+            <h3>{day}</h3>
+            <p>{time}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/pages/deliveryAddress/components/personalDetails/index.tsx
+++ src/pages/deliveryAddress/components/personalDetails/index.tsx
@@ -4,6 +4,15 @@ type Props = {}
 
 export const PersonalDetails = (_props: Props) => {
   return (
-    <div className={styles.Wrapper}>PersonalDetails</div>
-  )
+    <div className={styles.Wrapper}>
+      <h1>Personal Details</h1>
+      <input type="email" placeholder="Email" />
+      <div>
+        <input type="text" placeholder="First Name" />
+        <input type="text" placeholder="Last Name" />
+      </div>
+      <input type="text" placeholder="Mobile Number" />
+      <button>Save</button>
+    </div>
+  );
 }
