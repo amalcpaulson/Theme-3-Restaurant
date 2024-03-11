36b92a900d82be692b83a7c9b1a4532bd606e734
Jenin82 <jenin8282@gmail.com>
2024-03-11 22:16:01 +0530

    chore: fixed cart variable rendering issue

--- src/components/popups/cart/index.tsx
+++ src/components/popups/cart/index.tsx
@@ -27,112 +27,127 @@ interface RootState {
 }
 
 const Cart: React.FC = () => {
-  const cart = useSelector((state: RootState) => state.cart);
-  const dispatch = useDispatch();
+	const cart = useSelector((state: RootState) => state.cart);
+	const dispatch = useDispatch();
 
-  return (
-    <Popover.Root>
-      <Popover.Trigger asChild>
-        <button className={styles.iconTrigger} aria-label="Cart">
-          <CartSvg />
-          {cart.cart.length} Items
-        </button>
-      </Popover.Trigger>
-      <Popover.Anchor />
-      <Popover.Portal>
-        <Popover.Content className={styles.popoverContent}>
-          <div className={styles.Topset}>
-            <div>
-              <h2>Delivery</h2>
-              <p>Standard ( 40 min)</p>
-            </div>
-            <Popover.Close>×</Popover.Close>
-          </div>
-          <div className={styles.SelectedItems}>
-            {cart.cart.map((item) => (
-              <div className={styles.Individual} key={item.id}>
-                {" "}
-                <img src={item.img} className={styles.Image} alt="" />
-                <div>
-                  <div>
-                    {" "}
-                    <p>{item.name}</p>{" "}
-                    <button
-                      onClick={() =>
-                        dispatch(
-                          removeFromCart({
-                            id: item.id,
-                          })
-                        )
-                      }
-                    >
-                      ×
-                    </button>
-                  </div>
-                  <div>
-                    {" "}
-                    <span className="colorText">₹ {item.rate}.00</span>
-                    <div className={styles.Counter}>
-                      <button
-                        onClick={() =>
-                          dispatch(
-                            increaseItemQuantity({
-                              id: item.id,
-                            })
-                          )
-                        }
-                      >
-                        +
-                      </button>
-                      <p>{item.quantity}</p>
-                      <button
-                        onClick={() =>
-                          dispatch(
-                            decreaseItemQuantity({
-                              id: item.id,
-                            })
-                          )
-                        }
-                      >
-                        -
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-          <div>
-            <p>Any Suggestions ? We will pass it on</p>
-          </div>
-          <div>
-            <h2>Billing Details</h2>
-            <div>
-              <p>Item Total</p>
-              <p>₹160</p>
-            </div>
-            <div>
-              <p>Delivery Fee | 57.2kms</p>
-              <p>₹20</p>
-            </div>
-          </div>
-          <div>
-            <p>Delivery Tip</p>
-            <div>
-              <p>Gst and Restaurant Charges</p>
-              <p>₹15.89</p>
-            </div>
-          </div>
-          <div>
-            <h3>To Pay</h3>
-            <p>₹ {cart.totalAmount}.00</p>
-          </div>
-
-          <button>Go to checkout</button>
-        </Popover.Content>
-      </Popover.Portal>
-    </Popover.Root>
-  );
+	return (
+		<Popover.Root>
+			<Popover.Trigger asChild>
+				<button className={styles.iconTrigger} aria-label="Cart">
+					<CartSvg />
+					{cart.cart.length} Items
+				</button>
+			</Popover.Trigger>
+			<Popover.Anchor />
+			<Popover.Portal>
+				<Popover.Content className={styles.popoverContent}>
+					{cart.cart.length === 0 ? (
+						<div className={styles.EmptyCartMessage}>
+							<h2>Cart is empty</h2>
+							<p>Add something to it!</p>
+						</div>
+					) : (
+						<>
+							<div className={styles.Topset}>
+								<div>
+									<h2>Delivery</h2>
+									<p>Standard (40 min)</p>
+								</div>
+								<Popover.Close>×</Popover.Close>
+							</div>
+							<div className={styles.SelectedItems}>
+								{cart.cart.map((item) => (
+									<div
+										className={styles.Individual}
+										key={item.id}
+									>
+										<img
+											src={item.img}
+											className={styles.Image}
+											alt={item.name}
+										/>
+										<div>
+											<div>
+												<p>{item.name}</p>
+												<button
+													onClick={() =>
+														dispatch(
+															removeFromCart({
+																id: item.id,
+															})
+														)
+													}
+												>
+													×
+												</button>
+											</div>
+											<div>
+												<span className="colorText">
+													₹ {item.rate}.00
+												</span>
+												<div className={styles.Counter}>
+													<button
+														onClick={() =>
+															dispatch(
+																increaseItemQuantity(
+																	{
+																		id: item.id,
+																	}
+																)
+															)
+														}
+													>
+														+
+													</button>
+													<p>{item.quantity}</p>
+													<button
+														onClick={() =>
+															dispatch(
+																decreaseItemQuantity(
+																	{
+																		id: item.id,
+																	}
+																)
+															)
+														}
+													>
+														-
+													</button>
+												</div>
+											</div>
+										</div>
+									</div>
+								))}
+							</div>
+							<div>
+								<p>Any Suggestions? We will pass it on.</p>
+							</div>
+							<div>
+								<h2>Billing Details</h2>
+								<div>
+									<p>Item Total</p>
+									<p>₹{cart.totalAmount}</p>
+								</div>
+								<div>
+									<p>Delivery Fee | 57.2kms</p>
+									<p>₹20</p>
+								</div>
+								<div>
+									<p>Gst and Restaurant Charges</p>
+									<p>₹15.89</p>
+								</div>
+								<div>
+									<h3>To Pay</h3>
+									<p>₹{cart.totalAmount + 20 + 15.89}</p>
+								</div>
+							</div>
+							<button>Go to checkout</button>
+						</>
+					)}
+				</Popover.Content>
+			</Popover.Portal>
+		</Popover.Root>
+	);
 };
 
 export default Cart;
